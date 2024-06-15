bc26053f178d7b8e501fdf93b5a8f08953d7cbc2
hamesomer11 <hamzaomer45@gmail.com>
2024-06-15 21:10:46 +0100

    Form: Exercises 2.1/2.2 &2.3/2.4/2.5/2.6/2.7 - commit #2

--- src/App.jsx
+++ src/App.jsx
@@ -283,6 +283,59 @@ export default App
 
 */
 
+/**************************************** Exercise 2.6 **********************************************
+
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      content: newName,
+      important: Math.random() < 0.5,
+      id: persons.length + 1,
+  }
+    setPersons(persons.concat(nameObject))
+    console.log(persons, 'Checking persons array')
+    setNewName('')
+    console.log(newName, 'Checking newName variable')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person =>
+          <p key={person.id}>{person.content}</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+*/
+
+/******************************************** Exercise 2.7 **************************************** 
+
 import { useState } from 'react'
 import Person from './components/Person'
 
@@ -343,3 +396,5 @@ const App = () => {
 }
 
 export default App
+
+*/
