dcf3d02c4d42e8a422fe54414810fc790e2ed08a
hamesomer11 <hamzaomer45@gmail.com>
2024-07-07 23:55:57 +0100

    Data for countries: Exercise 2.20- commit #2

--- src/services/weather.js
+++ src/services/weather.js
@@ -5,7 +5,6 @@ const api_key = import.meta.env.VITE_WEATHER_API_KEY
 
 const getWeatherInfo = (latitude, longitude) => {
     const apiRequest = axios.get(`${baseUrl}/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api_key}`)
-    console.log(apiRequest, 'WTF!!!')
     return apiRequest.then(response => {
         console.log(`Server - SUCCESS: got weather info 
             for lat: ${latitude} & lon: ${longitude}`)
